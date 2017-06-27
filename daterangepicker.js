7d42e3ddb007c5f525f67f7f9e7d70183424b171
James Fefes <jfef@bn.co>
2017-06-27 10:48:05 -0400

    Logic fixes

--- daterangepicker.js
+++ daterangepicker.js
@@ -912,28 +912,30 @@
                 if (this.timePicker) {
                     if (this.startDate.isSame(this.ranges[range][0]) && this.endDate.isSame(this.ranges[range][1])) {
                         customRange = false;
-                        if (this.chosenLabel != "Custom Range") {
+                        if (this.chosenLabel == "Custom Range") {
                           this.chosenLabel = this.container.find('.ranges li:eq(' + i + ')')
                               .addClass('active').html();
                         }
-
-                        if (this.chosenLabel == this.container.find('.ranges li:eq(' + i + ')')[0].innerHTML){
-                          this.chosenLabel = this.container.find('.ranges li:eq(' + i + ')')
-                              .addClass('active').html();
+                        else{
+                          if (this.chosenLabel == this.container.find('.ranges li:eq(' + i + ')')[0].innerHTML){
+                            this.chosenLabel = this.container.find('.ranges li:eq(' + i + ')')
+                                .addClass('active').html();
+                          }
                         }
                     }
                 } else {
                     //ignore times when comparing dates if time picker is not enabled
                     if (this.startDate.format('YYYY-MM-DD') == this.ranges[range][0].format('YYYY-MM-DD') && this.endDate.format('YYYY-MM-DD') == this.ranges[range][1].format('YYYY-MM-DD')) {
                         customRange = false;
-                        if (this.chosenLabel != "Custom Range") {
+                        if (this.chosenLabel == "Custom Range") {
                           this.chosenLabel = this.container.find('.ranges li:eq(' + i + ')')
                               .addClass('active').html();
                         }
-
-                        if (this.chosenLabel == this.container.find('.ranges li:eq(' + i + ')')[0].innerHTML){
-                          this.chosenLabel = this.container.find('.ranges li:eq(' + i + ')')
-                              .addClass('active').html();
+                        else {
+                            if (this.chosenLabel == this.container.find('.ranges li:eq(' + i + ')')[0].innerHTML){
+                              this.chosenLabel = this.container.find('.ranges li:eq(' + i + ')')
+                                  .addClass('active').html();
+                            }
                         }
                     }
                 }
